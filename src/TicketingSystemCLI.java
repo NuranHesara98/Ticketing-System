d492f949391b5e89a098c492b5079ce1700a82cf
nuranhesara98 <nuranhesra321@gmail.com>
2024-11-25 16:30:48 +0530

    Create the Main Menu

--- src/TicketingSystemCLI.java
+++ src/TicketingSystemCLI.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
 public class TicketingSystemCLI {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        //Main Menu
+        while (!exit){
+            System.out.println("\n==========================================");
+            System.out.println("        Real-Time Ticketing System");
+            System.out.println("==========================================");
+            System.out.println("1. Start Ticketing System");
+            System.out.println("2. Stop Ticketing System");
+            System.out.println("3. View Real-Time Status");
+            System.out.println("4. View Logs");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    startTicketingSystem(scanner);
+                    break;
+                case 2:
+                    stopTicketingsystem();
+                    scanner.close();
+                    System.out.println("Exiting the system. GoodBye!");
+                    return;
+                case 3:
+                    viewRealTimeStatus();
+                    break;
+                case 4:
+                    viewLog();
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please select a valid option");
+
+            }
+        }
     }
 }
